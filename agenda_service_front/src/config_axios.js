bb09918d1626698c81ffaa38882d9265aca1ec8f
André de Souza Borges <andre_s_borges@estudante.sesisenai.org.br>
2024-07-11 21:59:06 -0300

    ajuste no cadastra usuario

--- agenda_service_front/src/config_axios.js
+++ agenda_service_front/src/config_axios.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 //Criar uma instância Axios com a Url do Backend
 // baseURL é a porta que está o backend
 export const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://localhost:8080'
 });
